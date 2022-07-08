a0c74de146074398afbce698032e4182d9a7c7ac
m0-dev <moritz.michel@online.de>
2022-07-08 19:59:37 +0200

    feat: add list of heroes and hero detail section
    * add custom hero styling

---
+++ ng-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero} from "./hero";
+
+export const HEROES: Hero[] = [
+  {id: 12, name: "FOO BAR"},
+  {id: 13, name: "Hackathon"},
+  {id: 14, name: "Rumble"},
+  {id: 15, name: "Dr. Frank"},
+  {id: 16, name: "Carl Cox"},
+  {id: 17, name: "Maria"},
+  {id: 18, name: "8aAd-!d"},
+  {id: 19, name: "Hello World!"},
+  {id: 20, name: "printf"},
+];
